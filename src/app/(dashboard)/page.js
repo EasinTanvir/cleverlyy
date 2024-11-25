a9ac6df030917ed3bf7535aaad54cca7ec480701
EasinTanvir <easintanvir102@gmail.com>
2024-11-26 00:14:43 +0600

    fixed an small issues

--- src/app/(dashboard)/page.js
+++ src/app/(dashboard)/page.js
@@ -5,8 +5,6 @@ import { getServerCredentials } from "../../../session/sersverSession";
 const DashboardPage = async () => {
   const session = await getServerCredentials();
 
-  throw new Error("failed");
-
   return (
     <div className="md:p-8 p-3 ">
       <Dashboard session={session} />
