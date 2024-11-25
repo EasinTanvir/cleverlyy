6bb7d8634f0b56a19ffb9ee7b4db460564c2c018
EasinTanvir <easintanvir102@gmail.com>
2024-11-25 22:45:33 +0600

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const headers = [
+  "Accept",
+  "Accept-Version",
+  "Content-Length",
+  "Content-MD5",
+  "Content-Type",
+  "Date",
+  "X-Api-Version",
+  "X-CSRF-Token",
+  "X-Requested-With",
+];
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -11,6 +24,24 @@ const nextConfig = {
         hostname: "your-s3-url",
       },
     ],
+  },
+
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://app.acme.local:3001",
+          },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: headers.join(", ") },
+        ],
+      },
+    ];
   },
 };
 
