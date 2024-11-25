e7e6ab3391fc37f4ac451eb3dac6e3cbb1bf6831
EasinTanvir <easintanvir102@gmail.com>
2024-11-25 23:17:40 +0600

    update not found page

--- src/app/(dashboard)/page.js
+++ src/app/(dashboard)/page.js
@@ -5,6 +5,8 @@ import { getServerCredentials } from "../../../session/sersverSession";
 const DashboardPage = async () => {
   const session = await getServerCredentials();
 
+  throw new Error("failed");
+
   return (
     <div className="md:p-8 p-3 ">
       <Dashboard session={session} />
